bda898e29c9d07f9894892e652f0819bb1f6d384
Ryan Seys <ryan@ryanseys.com>
2014-10-23 01:42:31 -0400

    Add comments for header

--- assign2/assign2.cpp
+++ assign2/assign2.cpp
@@ -1,3 +1,9 @@
+/**
+ * COMP 4002 - Assignment 2
+ * Task 1: Render a sphere at (100,10,100) using perspective projection.
+ *
+ * Author: Ryan Seys - 100817604
+ */
 #ifdef __APPLE_CC__
 #include <GLUT/glut.h>
 #else
@@ -39,10 +45,11 @@ int main(int argc, char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(800, 600);
-  glutCreateWindow("Ballin'");
+  glutCreateWindow("Task 1: Sphere");
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
-  // Look from point (200, 200, 200) at point (100, 10, 100) with the up vector (0, 1, 0)
+  // Look from point (200, 200, 200) at point (100, 10, 100)
+  // with the up vector (0, 1, 0)
   gluLookAt(200,200,200, 100,10,100, 0,1,0);
   glutPostRedisplay();
   glEnable(GL_DEPTH_TEST);
