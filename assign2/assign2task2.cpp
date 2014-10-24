69a3c8df8ff519aaa2b8fcc934e12360409a6b03
Ryan Seys <ryan@ryanseys.com>
2014-10-23 22:14:37 -0400

    Task 2 sorta

---
+++ assign2/assign2task2.cpp
@@ -0,0 +1,94 @@
+/**
+ * COMP 4002 - Assignment 2
+ * Task 1: Render a sphere at (100,10,100) using perspective projection.
+ *
+ * Author: Ryan Seys - 100817604
+ */
+#ifdef __APPLE_CC__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void drawCube(GLdouble width, GLdouble height, GLdouble depth) {
+  glPushMatrix();
+  glColor3f(0.0, 1.0, 1.0);
+  glScalef(width, height, depth);
+  glutWireCube(1.0);
+  glPopMatrix();
+}
+
+void drawSphere(GLdouble x, GLdouble y, GLdouble z, GLdouble radius) {
+  glPushMatrix();
+  //translates the current matrix to (100,10,100)
+  glTranslatef(x, y, z);
+  glColor3f(1.0, 1.0, 1.0);
+  glRotatef(-90.0, 1.0, 0.0, 0.0);
+  // draw the sphere
+  glutWireSphere(radius, 20, 20);
+  glPopMatrix();
+}
+
+/**
+ * Rendering the window.
+ */
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glMatrixMode(GL_MODELVIEW);
+  glPushMatrix();
+
+  drawCube(2.5, 1.0, 1.0);
+
+  // start sphere 1
+  drawSphere(-0.7,1.0,0.0, 0.5);
+  // end sphere 1
+
+  // start sphere 2
+  drawSphere(0.7,1.0,0.0, 0.5);
+
+  glPopMatrix();
+  // end object
+
+  glFlush();
+  glutSwapBuffers();
+}
+
+/**
+ * When the window reshapes to a new size
+ * @param w the window new width
+ * @param h the window new height
+ */
+void reshape(GLint w, GLint h) {
+  glViewport(0, 0, w, h);
+
+  // setting the current matrix mode to GL_PROJECTION
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  /** Fovy, aspect, zNear, zFar */
+  gluPerspective(60.0, (GLfloat)w/(GLfloat)h, 1.0, 400.0);
+  // reset the current matrix mode to GL_MODELVIEW to be used in display
+  glMatrixMode(GL_MODELVIEW);
+}
+
+/**
+ * Main.
+ */
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(800, 600);
+  glutCreateWindow("Task 2: Hierarchical Object");
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  // Look from point (200, 200, 200) at point (100, 10, 100)
+  // with the up vector (0, 1, 0)
+  gluLookAt(2,2,2, 0,0,0, 0,1,0);
+  glutPostRedisplay();
+  glEnable(GL_DEPTH_TEST);
+  // glEnable(GL_CULL_FACE);
+  // glCullFace(GL_BACK);
+  glutMainLoop();
+  return 0;
+}
